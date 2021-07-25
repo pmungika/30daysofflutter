9b36f2d0e9d1ce0e08c9ad853f4518d92fcdd2e0
Pushkar <pmungika@umich.edu>
2021-07-25 16:50:55 -0400

    Day 4 changes

---
+++ rouund2/lib/pages/login_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/gestures.dart';
+import "package:flutter/material.dart";
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image_ford.png",
+              fit: BoxFit.scaleDown,
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              "Welcome to Ford Adventure",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "User name",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("Let's Go");
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
