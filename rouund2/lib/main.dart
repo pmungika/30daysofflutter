02962a00c01dbfad3fd1479f9dd009f43d6a95a5
Pushkar <pmungika@umich.edu>
2021-07-26 23:41:10 -0400

    Day5 commit done

--- rouund2/lib/main.dart
+++ rouund2/lib/main.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import 'package:rouund2/pages/home_page.dart';
 import 'package:rouund2/pages/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rouund2/utils/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,14 +17,15 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.cyan,
         fontFamily: GoogleFonts.lato().fontFamily,
       ),
+      debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
       initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage()
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage()
       },
     );
   }
--- rouund2/lib/pages/login_page.dart
+++ rouund2/lib/pages/login_page.dart
@@ -1,61 +1,66 @@
 import 'package:flutter/gestures.dart';
 import "package:flutter/material.dart";
+import 'package:rouund2/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/login_image_ford.png",
-              fit: BoxFit.scaleDown,
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              "Welcome to Ford Adventure",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login_image_ford.png",
+                fit: BoxFit.scaleDown,
+                height: 240,
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter Username",
-                      labelText: "User name",
-                    ),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Enter Password",
-                      labelText: "Password",
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  ElevatedButton(
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(),
-                    onPressed: () {
-                      print("Let's Go");
-                    },
-                  )
-                ],
+              SizedBox(
+                height: 30.0,
               ),
-            )
-          ],
+              Text(
+                "Welcome to Ford Adventure",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter Username",
+                        labelText: "User name",
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter Password",
+                        labelText: "Password",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                    ),
+                    ElevatedButton(
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
